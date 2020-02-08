f6c38d23fab21ac835369483dccee2c51670439e
Alec Smecher <alec@smecher.bc.ca>
2020-02-07 16:52:41 -0800

    pkp/pkp-lib#5488 Update copyright dates and GPLv2 license to GPLv3

--- ReviewReportDAO.inc.php
+++ ReviewReportDAO.inc.php
@@ -3,9 +3,9 @@
 /**
  * @file plugins/reports/reviewReport/ReviewReportDAO.inc.php
  *
- * Copyright (c) 2014-2019 Simon Fraser University
- * Copyright (c) 2003-2019 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class ReviewReportDAO
  * @ingroup plugins_reports_review
