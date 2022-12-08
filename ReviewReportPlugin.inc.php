7831d66e4bf215e70f61da6e5f3f9990288bdc4a
Bozana Bokan <bozana.bokan@posteo.net>
2022-12-08 12:29:22 +0100

    pkp/pkp-lib#8397 add cancelled column to the review report

--- ReviewReportPlugin.inc.php
+++ ReviewReportPlugin.inc.php
@@ -123,6 +123,7 @@ class ReviewReportPlugin extends ReportPlugin
             'date_due' => __('reviewer.submission.reviewDueDate'),
             'overdue' => __('plugins.reports.reviews.reviewOverdue'),
             'declined' => __('submissions.declined'),
+            'cancelled' => __('common.cancelled'),
             'recommendation' => __('plugins.reports.reviews.recommendation'),
             'comments' => __('plugins.reports.reviews.comments')
         ];
@@ -156,6 +157,7 @@ class ReviewReportPlugin extends ReportPlugin
                     $columns[$index] = __(WorkflowStageDAO::getTranslationKeyFromId($row->$index));
                     break;
                 case 'declined':
+                case 'cancelled':
                     $columns[$index] = __($row->$index ? 'common.yes' : 'common.no');
                     break;
                 case 'unconsidered':
