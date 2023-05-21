e0cee750255900b37c852d5b40093568aeb57a15
Jonas Raoni Soares da Silva <github@raoni.org>
2023-05-21 21:00:48 +0300

    Added missing type hints

--- ReviewReportPlugin.php
+++ ReviewReportPlugin.php
@@ -22,7 +22,10 @@ use APP\core\Application;
 use PKP\core\PKPString;
 use PKP\db\DAORegistry;
 use PKP\plugins\ReportPlugin;
+use PKP\reviewForm\ReviewFormElementDAO;
+use PKP\reviewForm\ReviewFormResponseDAO;
 use PKP\submission\reviewAssignment\ReviewAssignment;
+use PKP\submission\reviewAssignment\ReviewAssignmentDAO;
 use PKP\workflow\WorkflowStageDAO;
 
 class ReviewReportPlugin extends ReportPlugin
